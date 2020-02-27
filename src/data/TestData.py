9cf2eb6775747d9f1633b7933df4b8c95866c10d
iulianstave <iulian.staverescu@gmail.com>
2020-02-27 13:37:28 +0200

    Add Charts test

--- src/data/TestData.py
+++ src/data/TestData.py
@@ -1,6 +1,7 @@
 class TestData():
     DATA_URL_EXT = "/data"
     CATALOGUE_URL_EXT = "/catalogue#/"
+    CHARTS_URL_EXT = "/data"
     SEARCH_TEXT_ATM = "air pollution"
     HOME_TITLE_ATM = "Copernicus"
     TOP_CARDS_ATM = ['Europe', 'Worldwide']
